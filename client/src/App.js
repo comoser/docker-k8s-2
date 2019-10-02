0927522dcd135f94e6529c764ca2fd6114d2203d
David Alecrim <d.alecrim@xgeeks.io>
2019-10-02 12:12:14 +0100

    feat: change text

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1>Fibonacci Sequence calculator</h1>
+          <h1>Fib calculator</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
